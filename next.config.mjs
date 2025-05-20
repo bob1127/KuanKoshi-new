a545b774e9e2314c03534614400150e6ceaf849d
bob1127 <61307295+bob1127@users.noreply.github.com>
2025-05-20 21:18:55 +0800

    eeeeee

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "**",
+        hostname: "inf.fjg.mybluehost.me",
       },
     ],
   },
