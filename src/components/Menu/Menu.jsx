bfa9e7b81f3f3d71c078a92d5f05d0b1e3544c4b
bob1127 <61307295+bob1127@users.noreply.github.com>
2025-05-26 09:35:36 +0800

    fgt

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -181,7 +181,7 @@ const Menu = ({ isDarkBg }) => {
         className="fixed top-0 left-0 w-full h-screen py-20 flex bg-[#375E77] z-[3] overflow-y-auto"
         style={{ clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)" }}
       >
-        <div className="w-[90%] max-w-[1400px] mx-auto flex flex-col justify-center items-center menu-content">
+        <div className="w-[90%] max-w-[1400px] mx-auto flex flex-col justify-center items-center menu-content min-h-screen">
           {/* Logo */}
           <div className="flex flex-col sm:flex-row sm:mt-[100px] mt-[20px] lg:mt-[130px] items-start md:items-center mb-0 sm:mb-12 px-6 w-full justify-start">
             <div className="flex items-center">
